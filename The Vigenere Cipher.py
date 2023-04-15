6a1b184b3f316fbdbf83ab918627d53c86547456
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-15 22:02:06 +0700

    Section for User Input. Done!

--- The Vigenere Cipher.py
+++ The Vigenere Cipher.py
@@ -13,6 +13,12 @@ print(
     )
 
 #Ask the user the message string and the key string
+print(Fore.BLUE + Style.BRIGHT + "✨ Message " + Fore.LIGHTCYAN_EX + Style.NORMAL + "\nKindly enter a plain text message in all uppercase letters with no spaces: ", end = "")
+message_input = input(Fore.WHITE + "")
+print(Fore.BLUE + Style.BRIGHT + "\n✨ Key " + Fore.LIGHTCYAN_EX + Style.NORMAL + "\nKindly enter a plain text key in all uppercase letters with no spaces: ", end = "")
+key_input = input(Fore.WHITE + "")
+
+
 #List the letter values that will be used in translating the plaintext inputted by the user
 #Use the number substitute to turn each element by its corresponding number
 #Get the sum of each elements from message and key which are aligned in column, then the mod 26 of each sum
