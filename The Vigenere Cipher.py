a827770b7cc85c79be8a4272f9a4b57577e7d14d
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-15 21:43:20 +0700

    Pseudocode. Done!

---
+++ The Vigenere Cipher.py
@@ -0,0 +1,10 @@
+#Title/Header Section
+#Welcome Message
+#Ask the user the message string and the key string
+#List the letter values that will be used in translating the plaintext inputted by the user
+#Use the number substitute to turn each element by its corresponding number
+#Get the sum of each elements from message and key which are aligned in column, then the mod 26 of each sum
+#Encrypt
+#Processing the Output/Loading
+#Display the decrypted text to the user
+#Ask the user if he/she wants to try again the program.
